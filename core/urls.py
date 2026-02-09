9ced78e97e0be37985e56bc952089088cd644e16
TURKI MOHAMMED SAAD ALMOULIY <443102104@student.ksu.edu.sa>
2026-02-09 10:19:17 +0300

    Add new dashboard routes for event and user actions

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.dashboard_view, name='home'),
 
     # Dashboard
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+    #path('dashboard/', views.dashboard_view, name='dashboard'),
 
     # Attendee Routes
     path('signup/', views.signup_attendee, name='signup_attendee'),
@@ -15,4 +15,23 @@ urlpatterns = [
     # Business Routes
     path('signup/business/', views.signup_business, name='signup_business'),
     path('login/business/', views.login_business, name='login_business'),
+
+    # The main hub
+    path('dashboard/', views.dashboard_redirect, name='dashboard'),
+
+    # Specific Dashboards
+    path('dashboard/organizer/', views.organizer_dashboard, name='organizer_dashboard'),
+    path('dashboard/vendor/', views.vendor_dashboard, name='vendor_dashboard'),
+
+    # Event Actions
+    path('dashboard/event/create/', views.create_event, name='create_event'),
+    path('dashboard/event/delete/<int:event_id>/', views.delete_event, name='delete_event'),
+
+    # SCEGA & Admin
+    path('dashboard/scega/', views.scega_dashboard, name='scega_dashboard'),
+    path('dashboard/event/<int:event_id>/approve/', views.approve_event, name='approve_event'),
+    path('dashboard/event/<int:event_id>/reject/', views.reject_event, name='reject_event'),
+
+    # Attendee
+    path('dashboard/attendee/', views.attendee_dashboard, name='attendee_dashboard'),
 ]
